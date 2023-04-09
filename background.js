f8708d75b84701715f9c32facd650185fe4ff000
Crowfunder <52300171+Crowfunder@users.noreply.github.com>
2023-04-10 00:24:36 +0200

    Add files via upload

---
+++ background.js
@@ -0,0 +1,20 @@
+/*
+	by Crowfunder
+	Github: https://github.com/Crowfunder  
+*/
+
+var trimmedUrl = "https://www.youtube.com/watch?v=";
+
+browser.menus.create({
+	id: "yt-open-embed-video",
+	title: "Open video as embed",
+	targetUrlPatterns: [trimmedUrl+'*']
+});
+
+browser.menus.onClicked.addListener(async function (info, tab) {
+	if (info.menuItemId == "yt-open-embed-video") {
+		let idUrl = info.linkUrl.replace(trimmedUrl, '');
+		let embedUrl = "https://www.youtube.com/embed/" + idUrl;
+		let newTab = await browser.tabs.create({ 'active': true, 'url': embedUrl, 'index': tab.index+1 });
+	}
+});
