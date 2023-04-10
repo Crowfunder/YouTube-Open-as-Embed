300d4daa1da7741e7b0be6c774a1c8a4af39a799
Crowfunder <52300171+Crowfunder@users.noreply.github.com>
2023-04-10 15:50:19 +0200

    Fix bug when opening a recommended vid from embed

--- background.js
+++ background.js
@@ -13,7 +13,7 @@ browser.menus.create({
 
 browser.menus.onClicked.addListener(async function (info, tab) {
 	if (info.menuItemId == "yt-open-embed-video") {
-		let idUrl = info.linkUrl.replace(trimmedUrl, '');
+		let idUrl = info.linkUrl.replace(trimmedUrl, '').split('&')[0];
 		let embedUrl = "https://www.youtube.com/embed/" + idUrl;
 		let newTab = await browser.tabs.create({ 'active': true, 'url': embedUrl, 'index': tab.index+1 });
 	}
